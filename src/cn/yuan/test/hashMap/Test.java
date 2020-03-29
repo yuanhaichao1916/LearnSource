115b46cf00243f742d4669c052eb94531cf97644
yuanhaichao <yhc_java@163.com>
2020-03-29 17:56:18 +0800

    JDK源码

---
+++ src/cn/yuan/test/hashMap/Test.java
@@ -0,0 +1,14 @@
+package cn.yuan.test.hashMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test {
+    public static void main(String[] args) {
+        Map<String,Double> hashMap = new HashMap<>();
+        hashMap.put("K1",0.1);
+        hashMap.put("K2",0.2);
+        hashMap.put("K3",0.3);
+        hashMap.put("K4",0.4);
+    }
+}
